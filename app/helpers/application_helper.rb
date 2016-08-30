760268a29e45b9425e83e83bd74b0ac37888b099
Purvi <purvi.agarwal@computechcorp.com>
2016-08-30 12:47:13 -0400

    Added Helper Method full_title for setting the WebPage Title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#Fucntion to check if the View provides a Title or not. If not, return a base title
+	def full_title(pageTitle = '')
+		baseTitle = "Ruby on Rails Tutorial Sample App"
+		if(pageTitle.empty?)
+			baseTitle
+		else
+			pageTitle + "|" + baseTitle
+		end
+	end
 end
