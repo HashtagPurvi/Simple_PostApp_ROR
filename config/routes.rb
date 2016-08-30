8acf5a5ed4a7fab63eb892cc85e4be07cf3ed83b
Purvi <purvi.agarwal@computechcorp.com>
2016-08-30 14:58:23 -0400

    Added a User Controller to track user related info

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   #Setting up the Root of the app
   root 'static_pages#home'	
 
@@ -7,6 +9,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about',to: 'static_pages#about'
   get '/contact',to: 'static_pages#contact'
+
+  get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
