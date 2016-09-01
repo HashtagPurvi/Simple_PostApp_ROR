fad3996e05a985e6cfb42033c1280ce9a3072932
Purvi <purvi.agarwal@computechcorp.com>
2016-09-01 17:05:51 -0400

    Added the User Resources for routes and Actions and Views for Show and Index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+  
+  #Creates a New User 
   def new
+  end
+
+  #Lists all the users
+  def index
+  	@users = User.all
+  end
+
+  # Shows a Particular User Page
+  def show
+  	@user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,8 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
 
+  #Adding RESTFUL routes for User Model
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
