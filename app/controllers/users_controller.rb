60c2f784057ef2a2dcc5672fa44e2c5aaa721a7e
Purvi <purvi.agarwal@computechcorp.com>
2016-09-02 11:42:48 -0400

    Added Sign Up Page functionalities for Unsucessfull attempt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,21 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  # Creates a New User Profile
+  def create
+  	@user = User.new(params[user_params])
+  	if @user.save
+  		#
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+	  def user_params
+	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	  end
+
+
 end
